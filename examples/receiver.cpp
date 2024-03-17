ae669f7e0d9dbd0ad08f28050d042bfbf6b59a5a
shkwon98 <seonghyeon.kwon@avikus.ai>
2024-03-17 15:59:44 +0000

    Add receiver and sender examples

---
+++ examples/receiver.cpp
@@ -0,0 +1,9 @@
+#include <robl.h>
+
+int main(int ac, char **av)
+{
+    ROBL::ROBL robl;
+    robl.Init(ac, av, "receiver");
+
+    return 0;
+}
--- examples/sender.cpp
+++ examples/sender.cpp
@@ -3,7 +3,7 @@
 int main(int ac, char **av)
 {
     ROBL::ROBL robl;
-    robl.Init(ac, av, "example_1");
+    robl.Init(ac, av, "sender");
 
     return 0;
 }
